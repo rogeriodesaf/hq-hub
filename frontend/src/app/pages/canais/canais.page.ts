37c601fb053b7f603beb01616dce7f1699602f69
Rogério de Sá <rogeriodesaf@gmail.com>
2026-06-26 10:53:28 -0300

    Adiciona imagens dos canais parceiros

--- frontend/src/app/pages/canais/canais.page.ts
+++ frontend/src/app/pages/canais/canais.page.ts
@@ -37,11 +37,31 @@ interface CanalHq {
 })
 export class CanaisPage {
   readonly canais: CanalHq[] = [
-    { nome: 'Nona Dimensao', url: 'https://www.youtube.com/@Nonadimens%C3%A3o', imagemUrl: null },
-    { nome: 'Colecionador por Hobby', url: 'https://www.youtube.com/@colecionadorporhobby', imagemUrl: null },
-    { nome: 'Na Minha Estante HQs', url: 'https://www.youtube.com/@NaMinhaEstanteHQs', imagemUrl: null },
-    { nome: 'Chiclete com Lombada', url: 'https://www.youtube.com/@Chicletecomlombada', imagemUrl: null },
-    { nome: 'Ola Mundo Geek', url: 'https://www.youtube.com/@Ol%C3%A1MundoGeek', imagemUrl: null },
+    {
+      nome: 'Nona Dimensao',
+      url: 'https://www.youtube.com/@Nonadimens%C3%A3o',
+      imagemUrl: 'https://yt3.googleusercontent.com/pqXz1nOsUDz1xMM2HaHoSkowspJWne2P9SD7EX2KlqznFE3QRmk2vlI6rfuaePR4auysLQCs-Q=s160-c-k-c0x00ffffff-no-rj',
+    },
+    {
+      nome: 'Colecionador por Hobby',
+      url: 'https://www.youtube.com/@colecionadorporhobby',
+      imagemUrl: 'https://yt3.googleusercontent.com/hTxlzDp_W7wCQ4HstjrRVjVkAhihQtpFUTDcAlX42fN9YrwACz6bsJLvixjmTVT05CTPT1OU_w=s160-c-k-c0x00ffffff-no-rj',
+    },
+    {
+      nome: 'Na Minha Estante HQs',
+      url: 'https://www.youtube.com/@NaMinhaEstanteHQs',
+      imagemUrl: 'https://yt3.googleusercontent.com/8BlqluYCyllUk7qsbLO3KzLAlR-giPXMRUAxwBy46Dr6-aTBjNAx9wLOOiGSR8UtRkQBFANu=s160-c-k-c0x00ffffff-no-rj',
+    },
+    {
+      nome: 'Chiclete com Lombada',
+      url: 'https://www.youtube.com/@Chicletecomlombada',
+      imagemUrl: 'https://yt3.googleusercontent.com/cbmwaFwmF3DB2zs8Wc2VbpH1trnlzz4hkh0GtprHVF2DxPsqezYj5F8bpwalH79epyloTpRDlA=s160-c-k-c0x00ffffff-no-rj',
+    },
+    {
+      nome: 'Ola Mundo Geek',
+      url: 'https://www.youtube.com/@Ol%C3%A1MundoGeek',
+      imagemUrl: 'https://yt3.googleusercontent.com/zK6K2vSmDIm9vaLZ4ljK_ro7ubPHMrFfSK3NE1aTlt1WxZCRCnxI-AfRY8YpQTjvNn-Ix-OLOQ=s160-c-k-c0x00ffffff-no-rj',
+    },
     { nome: 'Contraponto HQs', url: 'https://www.youtube.com/@contrapontohqs', imagemUrl: null },
   ];
 
